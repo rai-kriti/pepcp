8f7e9ac0efc339345ce9e88fdaf927858c764251
KRITI RAI <149457879+rai-kriti@users.noreply.github.com>
2026-06-30 15:52:04 +0530

    Time: 29 ms (99.14%), Space: 101.1 MB (94.5%) - LeetHub

--- 1943-describe-the-painting/1943-describe-the-painting.cpp
+++ 1943-describe-the-painting/1943-describe-the-painting.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
     vector<vector<long long>> splitPainting(vector<vector<int>>& s) {
-        long long p[(int)1e5+1]= {};
-        for(int i = 0; i<s.size(); i++){
+
+        int mx = 0;
+        for (auto &seg : s)
+            mx = max(mx, seg[1]);
+
+        vector<long long> p(mx + 1, 0);
+
+        for (int i = 0; i < s.size(); i++) {
             int l = s[i][0], r = s[i][1], c = s[i][2];
-            p[l]+=c;
-            p[r]-=c;
+            p[l] += c;
+            p[r] -= c;
         }
-        for(int i = 1; i<=1e5; i++) p[i]+=p[i-1];
-        for(int i = 0; i<s.size(); i++){
+
+        for (int i = 1; i <= mx; i++)
+            p[i] += p[i - 1];
+
+        for (int i = 0; i < s.size(); i++) {
             int l = s[i][0];
-            if(p[l] > 0) p[l]*=-1;
+            if (p[l] > 0)
+                p[l] *= -1;
         }
+
         vector<vector<long long>> ans;
-        long long mag = 0;
-        for(int i = 0; i<=1e5; i++){
-            if(p[i]!=0){
-                mag = abs(p[i]);
-                int j = i+1;
-                while(j<=1e5 && p[j]>0 && p[j] == mag)j++;
-                vector<long long> temp;
-                temp.push_back(i);
-                temp.push_back(j);
-                temp.push_back(mag);
-                ans.push_back(temp);
-                i = j-1;
+
+        for (int i = 0; i <= mx; i++) {
+            if (p[i] != 0) {
+                long long mag = abs(p[i]);
+                int j = i + 1;
+
+                while (j <= mx && p[j] > 0 && p[j] == mag)
+                    j++;
+
+                ans.push_back({(long long)i, (long long)j, mag});
+                i = j - 1;
             }
         }
+
         return ans;
     }
 };
