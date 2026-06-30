c368f077a6112edeb1f8b0bf436e558636366a76
KRITI RAI <149457879+rai-kriti@users.noreply.github.com>
2026-06-30 15:50:24 +0530

    Time: 117 ms (29.73%), Space: 119.6 MB (13.4%) - LeetHub

--- 1943-describe-the-painting/1943-describe-the-painting.cpp
+++ 1943-describe-the-painting/1943-describe-the-painting.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    vector<vector<long long>> splitPainting(vector<vector<int>>& segments) {
-        map<long long, long long> diff;
-
-        // Difference marking
-        for (auto &seg : segments) {
-            diff[seg[0]] += seg[2];
-            diff[seg[1]] -= seg[2];
+    vector<vector<long long>> splitPainting(vector<vector<int>>& s) {
+        long long p[(int)1e5+1]= {};
+        for(int i = 0; i<s.size(); i++){
+            int l = s[i][0], r = s[i][1], c = s[i][2];
+            p[l]+=c;
+            p[r]-=c;
         }
-
+        for(int i = 1; i<=1e5; i++) p[i]+=p[i-1];
+        for(int i = 0; i<s.size(); i++){
+            int l = s[i][0];
+            if(p[l] > 0) p[l]*=-1;
+        }
         vector<vector<long long>> ans;
-
-        long long sum = 0;
-        long long start = -1;
-
-        for (auto &it : diff) {
-            long long end = it.first;
-
-            // Previous segment
-            if (start != -1 && sum != 0) {
-                ans.push_back({start, end, sum});
+        long long mag = 0;
+        for(int i = 0; i<=1e5; i++){
+            if(p[i]!=0){
+                mag = abs(p[i]);
+                int j = i+1;
+                while(j<=1e5 && p[j]>0 && p[j] == mag)j++;
+                vector<long long> temp;
+                temp.push_back(i);
+                temp.push_back(j);
+                temp.push_back(mag);
+                ans.push_back(temp);
+                i = j-1;
             }
-
-            // Prefix sum update
-            sum += it.second;
-
-            // Next segment starts from current point
-            start = end;
         }
-
         return ans;
     }
 };
