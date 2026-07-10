58721e90e070b9cefb01d08fc4a9ddfe7ede8cf2
KRITI RAI <149457879+rai-kriti@users.noreply.github.com>
2026-07-10 12:07:54 +0530

    Time: 139 ms (61.72%), Space: 35.2 MB (14.56%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     vector<vector<int>> dp;
 
-    int f(vector<vector<int>>& piles, int ind, int k) {
-        if (k == 0) return 0;
+    int f(vector<vector<int>>& pref, int ind, int k) {
+        if (k == 0)
+            return 0;
 
-        if (ind == piles.size()) {
-            return -1e9;
-        }
+        if (ind == pref.size())
+            return -1000000000;
 
         if (dp[ind][k] != -1)
             return dp[ind][k];
 
-        int ans = f(piles, ind + 1, k);
+        // Take 0 coins from current pile
+        int ans = f(pref, ind + 1, k);
 
-        int sum = 0;
+        int limit = min(k, (int)pref[ind].size() - 1);
 
-        for (int i = 0; i < min(k, (int)piles[ind].size()); i++) {
-            sum += piles[ind][i];
-            ans = max(ans, sum + f(piles, ind + 1, k - i - 1));
+        // Try taking 1,2,3... coins
+        for (int take = 1; take <= limit; take++) {
+            ans = max(ans,
+                      pref[ind][take] + f(pref, ind + 1, k - take));
         }
 
         return dp[ind][k] = ans;
@@ -26,7 +28,20 @@ public:
 
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         int n = piles.size();
+
+        // Prefix sums
+        vector<vector<int>> pref(n);
+
+        for (int i = 0; i < n; i++) {
+            pref[i].push_back(0);
+
+            for (int coin : piles[i]) {
+                pref[i].push_back(pref[i].back() + coin);
+            }
+        }
+
         dp.assign(n, vector<int>(k + 1, -1));
-        return f(piles, 0, k);
+
+        return f(pref, 0, k);
     }
 };
