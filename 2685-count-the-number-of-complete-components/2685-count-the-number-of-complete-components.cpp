564c74167fe260b0a2b81ec77dde4e83ce3fe7a7
KRITI RAI <149457879+rai-kriti@users.noreply.github.com>
2026-07-12 01:07:14 +0530

    Time: 43 ms (64.66%), Space: 128.5 MB (71.93%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+int dfs(int node , vector<vector<int>> &adj , vector<int> &visited ,int &edgessum){
+        visited[node] = 1;
+        edgessum +=  adj[node].size();
+
+        int currnodes =1;
+
+        for( int nei :  adj[node]){
+            if(!visited[nei]){
+                currnodes += dfs(nei , adj , visited, edgessum);
+            }
+        }
+
+        return currnodes;
+}
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+
+        for(auto &ed : edges){
+            adj[ed[0]].push_back(ed[1]);
+            adj[ed[1]].push_back(ed[0]);
+        }
+        int ans =0 ;
+        vector<int> visited(n , 0);
+        for(int i=0 ; i<n ; i++){
+            if(visited[i]) continue;
+
+            int edgessum =0;
+            int nodes =  dfs(i , adj , visited , edgessum);
+
+           int newedgessum  =  edgessum/2;
+
+            if(newedgessum == nodes*(nodes-1)/2 ) 
+                ans++  ;
+        }
+        
+    return ans;
+    }
+
+};
