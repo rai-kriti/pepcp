eaa51ea02bc8bdfde8d7437b6b319285625e5d0e
KRITI RAI <149457879+rai-kriti@users.noreply.github.com>
2026-07-15 00:39:31 +0530

    Time: 818 ms (61.74%), Space: 421.1 MB (5.22%) - LeetHub

---
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int recu(int i, int g1, int g2, vector<int>& nums,vector<vector<vector<int>>>& dp) {
+        int n = nums.size();
+        // base case
+        if (i == n) {
+            if (g1 == g2 && g1 != 0 && g2 != 0)
+                return 1;
+
+            // agar last me phoch gye pr condition nhi satisfy hua to 0
+            return 0;
+        }
+
+        if (dp[i][g1][g2] != -1)
+            return dp[i][g1][g2];
+
+        int ans = 0;
+
+        ans = (ans + recu(i + 1, g1, g2, nums , dp)) % mod;
+        ans = (ans + recu(i + 1, gcd(g1, nums[i]), g2, nums , dp)) % mod;
+
+        ans = (ans + recu(i + 1, g1, gcd(g2, nums[i]), nums , dp)) % mod;
+
+        return dp[i][g1][g2] = ans;
+    }
+    int subsequencePairCount(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(201, vector<int>(201, -1)));
+
+        return recu(0, 0, 0, nums , dp);
+    }
+};
