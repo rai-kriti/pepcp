c79799c74efa5c0d68f7aa472c0929d6a6d9ac19
KRITI RAI <149457879+rai-kriti@users.noreply.github.com>
2026-09-09 02:27:09 +0530

    Time: 0 ms (100%), Space: 13.4 MB (35.74%) - LeetHub

--- 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
+++ 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
@@ -1,16 +1,53 @@
 class Solution {
 public:
-   int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
-	int ans = 0;
-	vector<int> pos;
-	for(int i=0;i<students.size();i++) pos.push_back(i);
-	do{
-		int curr = 0;
-		for(int i = 0;i<students.size(); i++)
-			for(int j=0;j<students[pos[i]].size();j++)
-				curr += (students[pos[i]][j] == mentors[i][j]);
-		ans = max(ans, curr);
-	} while(next_permutation(pos.begin(), pos.end()) );
-	return ans;
-}
+
+    int maxCompatibilitySum(vector<vector<int>>& students,
+                            vector<vector<int>>& mentors) {
+
+        int m = students.size();
+
+        // score[i][j] = compatibility of student i with mentor j
+        vector<vector<int>> score(m, vector<int>(m));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+
+                for (int k = 0; k < students[i].size(); k++) {
+                    if (students[i][k] == mentors[j][k]) {
+                        score[i][j]++;
+                    }
+                }
+            }
+        }
+
+        // dp[mask] = maximum score after using mentors in mask
+        vector<int> dp(1 << m, 0);
+
+        for (int mask = 0; mask < (1 << m); mask++) {
+
+            // Number of students already assigned
+            int student = __builtin_popcount(mask);
+
+            // All students assigned
+            if (student == m)
+                continue;
+
+            // Try every unused mentor
+            for (int mentor = 0; mentor < m; mentor++) {
+
+                // mentor is already used
+                if (mask & (1 << mentor))
+                    continue;
+
+                int newMask = mask | (1 << mentor);
+
+                dp[newMask] = max(
+                    dp[newMask],
+                    dp[mask] + score[student][mentor]
+                );
+            }
+        }
+
+        return dp[(1 << m) - 1];
+    }
 };
