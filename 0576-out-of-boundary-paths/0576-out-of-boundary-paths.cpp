87579bf297ad73d40aee57ba73d17b2e3d910207
KRITI RAI <149457879+rai-kriti@users.noreply.github.com>
2026-09-18 23:11:41 +0530

    Time: 18 ms (8.33%), Space: 12.2 MB (25%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -1,30 +1,28 @@
 class Solution {
-    int mod = 1e9 + 7;
-    int memo[51][51][51];
-
-    int solve(int r, int c, int m, int n, int movesLeft) {
-        // Base Case 1: If we step out of bounds, we found 1 valid path!
-        if (r < 0 || r >= m || c < 0 || c >= n) return 1;
-        
-        // Base Case 2: Out of moves but still inside the grid
-        if (movesLeft == 0) return 0;
-
-        // Return cached result if already computed
-        if (memo[r][c][movesLeft] != -1) return memo[r][c][movesLeft];
-
-        //dfs
-        long long paths = 0;
-        paths = (paths + solve(r - 1, c, m, n, movesLeft - 1)) % mod; // Up
-        paths = (paths + solve(r + 1, c, m, n, movesLeft - 1)) % mod; // Down
-        paths = (paths + solve(r, c - 1, m, n, movesLeft - 1)) % mod; // Left
-        paths = (paths + solve(r, c + 1, m, n, movesLeft - 1)) % mod; // Right
-
-        return memo[r][c][movesLeft] = paths;
-    }
-
 public:
+    vector<vector<int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    const int M = 1e9 + 7;
+
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        memset(memo, -1, sizeof(memo));
-        return solve(startRow, startColumn, m, n, maxMove);
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(maxMove+1, 0)));
+
+        for(int k = 1; k <= maxMove; k++){
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    for(auto &ele : dir){
+                        int new_r = i + ele[0];
+                        int new_c = j + ele[1];
+
+                        if(new_r < 0 || new_r >= m || new_c < 0 || new_c >= n) {
+                            dp[i][j][k] = (dp[i][j][k] + 1) % M;
+                        } else {
+                            dp[i][j][k] = (dp[i][j][k] + dp[new_r][new_c][k-1]) % M;
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[startRow][startColumn][maxMove];
     }
 };
