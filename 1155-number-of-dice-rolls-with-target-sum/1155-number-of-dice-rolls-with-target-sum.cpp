edef35f6868eb160e03fb2bb123b1dba6286e863
KRITI RAI <149457879+rai-kriti@users.noreply.github.com>
2026-09-19 00:19:40 +0530

    Time: 26 ms (18.92%), Space: 11.1 MB (78.91%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -4,26 +4,29 @@ public:
 
         int MOD = 1000000007;
 
-        vector<vector<int>> dp(n + 1,
-                               vector<int>(target + 1, 0));
+        vector<int> dp(target + 1, 0);
 
-        dp[0][0] = 1;
+        dp[0] = 1;
 
         for (int dice = 1; dice <= n; dice++) {
+
+            vector<int> newdp(target + 1, 0);
 
             for (int sum = 1; sum <= target; sum++) {
 
                 for (int face = 1; face <= k; face++) {
 
                     if (sum >= face) {
-                        dp[dice][sum] =
-                            (dp[dice][sum] +
-                             dp[dice - 1][sum - face]) % MOD;
+                        newdp[sum] =
+                            (newdp[sum] +
+                             dp[sum - face]) % MOD;
                     }
                 }
             }
+
+            dp = newdp;
         }
 
-        return dp[n][target];
+        return dp[target];
     }
 };
